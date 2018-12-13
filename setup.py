30208c9385088a193159a9dee55bf77151c8e919
Augusto Destrero <adestrero@gmail.com>
2018-12-13 17:39:09 +0100

    use div instead of p

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-lazycrud',
-    version = '1.4.5',
+    version = '1.4.6',
     packages = find_packages(),
     author = 'Augusto Destrero',
     author_email = 'a.destrero@gmail.com',
