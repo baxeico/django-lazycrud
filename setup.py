b25889573c938d0a5d7d1850b4ec20bb607573f0
Augusto Destrero <adestrero@gmail.com>
2019-01-21 12:51:16 +0100

    added pre_table and post_table blocks

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-lazycrud',
-    version = '1.4.6',
+    version = '1.4.7',
     packages = find_packages(),
     author = 'Augusto Destrero',
     author_email = 'a.destrero@gmail.com',
