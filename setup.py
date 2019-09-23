c3a940219793adfdfd3d44f3ff9ba67cb625736f
Augusto Destrero <adestrero@gmail.com>
2019-09-23 18:12:57 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-lazycrud',
-    version = '1.5.7',
+    version = '1.5.8',
     packages = find_packages(),
     author = 'Augusto Destrero',
     author_email = 'a.destrero@gmail.com',
