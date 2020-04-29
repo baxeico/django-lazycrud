2398f27d0a5728edbbb6f78d877eb053dc844cb5
Augusto Destrero <a.destrero@gmail.com>
2020-04-29 18:42:36 +0200

    Version 1.6.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-lazycrud',
-    version = '1.6.1',
+    version = '1.6.2',
     packages = find_packages(),
     author = 'Augusto Destrero',
     author_email = 'a.destrero@gmail.com',
