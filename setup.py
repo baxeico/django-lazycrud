0efd35a983ddafbb42fe884bdde0cb9161be0119
Augusto Destrero <a.destrero@gmail.com>
2022-03-31 16:28:36 +0200

    Respect form and formset helpers, if configured

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-lazycrud',
-    version = '1.7.3',
+    version = '1.7.4',
     packages = find_packages(),
     author = 'Augusto Destrero',
     author_email = 'a.destrero@gmail.com',
