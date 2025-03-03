b15fec4f1205e27cd1d68bab619283a0c52c71dc
Augusto Destrero <a.destrero@gmail.com>
2025-03-03 13:17:13 +0100

    Remove Catalan translation for dataTables

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-lazycrud',
-    version = '1.8.2',
+    version = '1.8.3',
     packages = find_packages(),
     author = 'Augusto Destrero',
     author_email = 'a.destrero@gmail.com',
